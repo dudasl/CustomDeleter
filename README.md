cf695d0ee8aa6518961f633ab62c2a33247e6f46
Ladislav Dudáš <laco.dudas@nitropdf.com>
2016-10-25 21:16:00 +0200

    Custom deleter of 'my_ptr' implemented by `std::bind` and `struct`

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # CustomDeleter
-Using custom deleter with additional param
+This project shows how to use custom deleter with additional param
+
+## Problem
+- derive own class from `std::unique_ptr` (in this example `my_ptr` that holds pointer to `int`)
+- ctor of class have two parameters:
+    1. `int* p` - pointer to holded value
+    2. `int x` - additional param to deleter
+- deleter of that needs one additional param (in this example `int x`)
+- implement class that hide deleter from outside code and class also should not be template, e.g. `my_ptr ptr(new int(10), 20);` should be compilable
+- code should be c++11/14 compliant
+
+## Solutions
+Solutions should be based on macro `MY_PTR_BASED_CODE` which can be defined as:
+- `BIND_BASED_CODE` (1) solution based on binding argument to deleter function
+- `DELETER_BASED_CODE` (2) solution based on deleter defined as struct
+- `LAMBDA_BASED_CODE` (3) solution based on lambda function as deleter
---
+++ my_ptr.cpp
@@ -0,0 +1,89 @@
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+#include <memory>
+#include <functional>
+
+#define BIND_BASED_CODE		1
+#define DELETER_BASED_CODE	2
+#define LAMBDA_BASED_CODE	3
+#define MY_PTR_BASED_CODE	LAMBDA_BASED_CODE
+
+#if MY_PTR_BASED_CODE == BIND_BASED_CODE
+void my_free(int x, int* p)
+{
+	std::cout << "my_free - *p: " << *p << "\nx: " << x << "\n";
+
+	delete p;
+}
+
+class my_ptr : public std::unique_ptr<int, std::_Bind<true, void, void(*const)(int, int *), const int&, std::_Ph<1> &>>
+{
+public:
+	my_ptr(int* p, const int x) : std::unique_ptr<int, std::_Bind<true, void, void(*const)(int, int *), const int&, std::_Ph<1> &>>(p, std::bind(my_free, x, std::placeholders::_1)) {}
+
+	my_ptr(my_ptr&& other) : std::unique_ptr<int, std::_Bind<true, void, void(*const)(int, int *), const int&, std::_Ph<1> &>>(std::move(other)) {}
+};
+#elif MY_PTR_BASED_CODE == DELETER_BASED_CODE
+class my_ptr_deleter {
+private:
+	int _x;
+
+public:
+	explicit my_ptr_deleter(int x) : _x(x) {}
+
+	void operator() (int* p) { std::cout << "my_ptr_deleter - *p: " << *p << "\nx: " << this->_x << '\n'; delete p; }
+};
+
+class my_ptr : public std::unique_ptr<int, my_ptr_deleter>
+{
+public:
+	my_ptr(int* p, const int x);
+
+	my_ptr(my_ptr&& other) : std::unique_ptr<int, my_ptr_deleter>(std::move(other)) {}
+};
+
+my_ptr::my_ptr(int* p, const int x) : std::unique_ptr<int, my_ptr_deleter>(p, my_ptr_deleter(x))
+{
+}
+#elif MY_PTR_BASED_CODE == LAMBDA_BASED_CODE
+#endif
+
+my_ptr create_my_ptr(int initVal, int x)
+{
+	my_ptr ptr(new int(initVal), x);
+
+	return ptr;
+}
+
+class my_trivial_ptr : public std::unique_ptr<int>
+{
+public:
+	my_trivial_ptr(int initVal) : std::unique_ptr<int>(new int(initVal)) {}
+
+	my_trivial_ptr(my_trivial_ptr&& other) : std::unique_ptr<int>(std::move(other)) {}
+};
+
+my_trivial_ptr create_trivial_ptr(int initVal)
+{
+	my_trivial_ptr ptr(initVal);
+
+	return ptr;
+}
+
+int main(int argc, char * argv[])
+{
+	//int * raw_ptr = new int(10);
+	//auto my_deleter = std::bind(my_free, std::placeholders::_1, 2);
+	//auto my_lambda = [](int* t) { my_free(3, t); };
+	
+	//std::unique_ptr<int, std::_Bind<true, void, void(*const)(int *, int), std::_Ph<1> &, int>> ptr(raw_ptr, std::bind(my_free, std::placeholders::_1, 2));
+	//std::unique_ptr<int, void(*)(int *)> ptr2(new int(10), [](int* t) { my_free(3, t); });
+	my_ptr ptr1(create_my_ptr(10, 20));
+	my_ptr ptr2(new int(30), 60);
+
+	//my_trivial_ptr ptr(create_trivial_ptr(5));
+
+	return 0;
+}
